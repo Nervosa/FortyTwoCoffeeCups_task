4bd1580d197bdd6dc7d42b10a546ccddcbba50ec
nervosa <Nervosa@tut.by>
2013-11-28 10:17:51 +0300

    - tests added.

--- FortyTwoCoffeeCups/tests.py
+++ FortyTwoCoffeeCups/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from FortyTwoCoffeeCups.models import PersonBio
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class PersonBioTest(TestCase):
+
+    def test_homepage(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No BIOs available.')
+        new_bio = PersonBio(name='Test Name',
+                            surname='Test Surname',
+                            date_of_birth='2000-01-01',
+                            bio='Test Bio',
+                            email='TestEmail@test.com',
+                            skype_id='TestSkype',
+                            jabber_id='TestJabber@jabber.co',
+                            other_contacts='Test Other Contacts')
+        new_bio.save()
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'TestJabber@jabber.co')
