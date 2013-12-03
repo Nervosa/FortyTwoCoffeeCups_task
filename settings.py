6e618fcad28dd0a8c939027dc28cd136dbb6165f
nervosa <Nervosa@tut.by>
2013-12-03 11:26:37 +0300

    - ticket: 4 context processor is added to settings.

--- settings.py
+++ settings.py
@@ -43,6 +43,19 @@ INSTALLED_APPS = (
     'annoying',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+    'FortyTwoCoffeeCups.c_context_processors.add_settings_to_context',
+
+)
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
