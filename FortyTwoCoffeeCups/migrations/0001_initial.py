22ec31e8893a5547492e1a7642dc6cab267e0810
nervosa <Nervosa@tut.by>
2013-12-08 20:54:21 +0300

    - Commit to a fifth branch.

--- FortyTwoCoffeeCups/migrations/0001_initial.py
+++ FortyTwoCoffeeCups/migrations/0001_initial.py
@@ -18,17 +18,39 @@ class Migration(SchemaMigration):
             ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
             ('skype_id', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('jabber_id', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('photo', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True)),
             ('other_contacts', self.gf('django.db.models.fields.TextField')()),
         ))
         db.send_create_signal(u'FortyTwoCoffeeCups', ['PersonBio'])
+
+        # Adding model 'Http_Request_for_DB'
+        db.create_table(u'FortyTwoCoffeeCups_http_request_for_db', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('server_name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('server_port', self.gf('django.db.models.fields.CharField')(max_length=15)),
+            ('other_info', self.gf('django.db.models.fields.CharField')(max_length=5000)),
+        ))
+        db.send_create_signal(u'FortyTwoCoffeeCups', ['Http_Request_for_DB'])
+
 
     def backwards(self, orm):
         # Deleting model 'PersonBio'
         db.delete_table(u'FortyTwoCoffeeCups_personbio')
 
+        # Deleting model 'Http_Request_for_DB'
+        db.delete_table(u'FortyTwoCoffeeCups_http_request_for_db')
+
+
     models = {
+        u'FortyTwoCoffeeCups.http_request_for_db': {
+            'Meta': {'object_name': 'Http_Request_for_DB'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'other_info': ('django.db.models.fields.CharField', [], {'max_length': '5000'}),
+            'server_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'server_port': ('django.db.models.fields.CharField', [], {'max_length': '15'})
+        },
         u'FortyTwoCoffeeCups.personbio': {
-            'Meta': {'object_name': 'PersonBio'},
+            'Meta': {'ordering': "['id']", 'object_name': 'PersonBio'},
             'bio': ('django.db.models.fields.TextField', [], {}),
             'date_of_birth': ('django.db.models.fields.DateField', [], {}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
@@ -36,6 +58,7 @@ class Migration(SchemaMigration):
             'jabber_id': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'other_contacts': ('django.db.models.fields.TextField', [], {}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True'}),
             'skype_id': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'surname': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         }
